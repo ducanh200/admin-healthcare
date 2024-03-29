b5d88aa2120217edd848397db6d9f90c731166c1
Hieunmb <zhuongdae@gmail.com>
2024-03-29 14:38:04 +0700

    login

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import Dashbroad from './components/pages/dashbroad';
 import Header from './components/layouts/header';
 import Sidebar from './components/layouts/sidebar';
+import Login from './components/pages/login';
 
 
 function App() {
+  const location = useLocation();
+
+  const isHomeRoute = () => {
+    return location.pathname === '/login';
+  };
+
   return (
-      <div class="main-wrapper">
-        <Header/>
-        <Sidebar/>
-        <Routes>
-          <Route path='/' element={<Dashbroad/>}/>
-        </Routes>
-      </div>
+    <div>
+      {!isHomeRoute() && <Header currentLocation={location.pathname}/>}
+      {!isHomeRoute() && <Sidebar currentLocation={location.pathname}/>}
+      <Routes>
+        <Route path='login' element={<Login/>}/>
+        <Route path='/' element={<Dashbroad/>}/>
+      </Routes>
+    </div>
   );
 }
 
